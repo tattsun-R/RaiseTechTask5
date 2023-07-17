b37a1743938896397c8132a68f7b5c1736b4d9bb
tattsun-R <tattsun178@gmail.com>
2023-07-17 18:11:43 +0900

    第5回課題提出

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] count = new int[6];
+        int totalRolls = 500;
+
+        for (int i = 0; i < totalRolls; i++) {
+            int diceRoll = random.nextInt(6) + 1;
+            count[diceRoll - 1]++;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            int diceNumber = i + 1;
+            int diceCount = count[i];
+            double probability = (double) diceCount / totalRolls * 100;
+
+            System.out.println("出目 " + diceNumber + ": " + diceCount + " 回, 確率 " + probability + "%");
+        }
+    }
+}
